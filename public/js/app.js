a6f8b47b320b8a51205f47ca615ebabeaf3fdac8
fnascimento <felipe.basina@gmail.com>
2019-10-23 22:21:39 -0300

    Adicionado configurações para permitir deployment no heroku.

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ cotacoesForm.addEventListener('submit', (event) => {
         return;
     }
 
-    fetch(`http://localhost:3000/cotacoes?ativo=${ativo}`).then((response) => {
+    fetch(`/cotacoes?ativo=${ativo}`).then((response) => {
         response.json().then((data) => {
             if (data.message) {
                 mainMensage.innerText = `${data.message}, código ${data.code}`
--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.get('*', (request, response) => {
     });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`); 
 });
